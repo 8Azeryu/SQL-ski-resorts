4227d156f725141482f127231904a34f070e825b
Dominik Krawczyk <150517712+8Azeryu@users.noreply.github.com>
2024-03-04 10:03:34 +0100

    Delete analysed_csv directory

--- analysed_csv/difference.sql
+++
@@ -1,6 +0,0 @@
-SELECT 
-	id
-	,Resort 
-	,`Highest slope` - `Lowest point` 			as lowest_and_highest_points_difference
-	FROM resorts r 
-order by lowest_and_highest_points_difference DESC
--- analysed_csv/highest_mountains.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-	id
-	,Resort 
-	,`Highest slope` 
-	,`Lowest point` 
-FROM resorts r 
-order by `Highest slope`  DESC 
-limit 17
